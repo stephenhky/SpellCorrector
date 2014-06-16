11ee452f784eeef174366b374f23e08eb68f2baa
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2014-06-16 15:48:16 -0400

    exceptions handled

---
+++ src/main/scala/home/kwyho/spellcheck/home/kwyho/spellcheck/test/SpellCorrectionTester.scala
@@ -0,0 +1,28 @@
+package home.kwyho.spellcheck.home.kwyho.spellcheck.test
+
+/**
+ * Created by hok1 on 6/16/14.
+ */
+
+import home.kwyho.spellcheck.SpellCorrector
+import java.io.File
+import java.lang.UnsupportedOperationException
+
+object SpellCorrectionTester {
+  def main(args : Array[String]) = {
+    println("Initializing spell-check engine...")
+    val spellCorrector = new SpellCorrector()
+    println("Loading dictionary...")
+    spellCorrector.train(new File("big.txt"))
+    var spelling : String = readLine("spelling> ")
+    while (spelling != "") {
+      try {
+        println(spellCorrector.correct(spelling))
+      } catch {
+        case uoe : UnsupportedOperationException => println("no spell correction available!!")
+        case e : Exception => println("Unknown exception!!")
+      }
+      spelling = readLine("spelling> ")
+    }
+  }
+}
